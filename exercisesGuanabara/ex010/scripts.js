82f2eb17b31404706576d882e2eadcda52f72fdd
Agny Fonseca <70056921+AgnyFonseca@users.noreply.github.com>
2020-10-30 23:02:51 +0100

    exercisesGuanabara

---
+++ exercisesGuanabara/ex010/scripts.js
@@ -0,0 +1,18 @@
+let answer = window.document.getElementById('output')
+
+function action1() {
+    answer.innerHTML += '<p>The first button was clicked.</p>'
+}
+
+function action2() {
+    answer.innerHTML += '<p>The second button was clicked.</p>'
+}
+
+function action3() {
+    answer.innerHTML += '<p>The third button was clicked.</p>'
+}
+
+function action4() {
+    answer.innerHTML += '<p>The fourth button was clicked.</p>'
+}
+
